ee7bba3e386ffdd6066313bc8b3b41ea8c4aee49
radicalten <79894181+radicalten@users.noreply.github.com>
2025-09-13 17:42:22 -0500

    Update main.c

--- src9-2/main.c
+++ src9-2/main.c
@@ -1,7 +1,7 @@
 #include <SDL2/SDL.h>
 //#include <SDL2/SDL_error.h>
 //#include <SDL2/SDL_events.h>
-#include <SDL2/SDL_keysym.h>
+//#include <SDL2/SDL_keysym.h>
 #include <SDL2/SDL_mixer.h>
 #include <stdio.h>
 #include <stdlib.h>
