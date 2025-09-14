4d0e691b9689d4a13283d82c9a9d625b08fd2024
radicalten <79894181+radicalten@users.noreply.github.com>
2025-09-13 20:40:50 -0500

    Update main.h

--- src9-3/main.h
+++ src9-3/main.h
@@ -4,7 +4,7 @@
 #include "ch8.h"
 
 bool window_create();
-void window_process_input(CH8_Core *cpu);
-void window_render(CH8_Core *cpu);
+void window_process_input(struct CH8_Core *cpu);
+void window_render(struct CH8_Core *cpu);
 
 #endif // MAIN_H
